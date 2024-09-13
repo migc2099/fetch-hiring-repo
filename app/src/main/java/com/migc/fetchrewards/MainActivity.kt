a49732560b18ca333a21b8c4022b41503e666b2a
Miguel Cavero <migc.dev@gmail.com>
2024-09-13 11:48:02 -0500

    display of list done

--- app/src/main/java/com/migc/fetchrewards/MainActivity.kt
+++ app/src/main/java/com/migc/fetchrewards/MainActivity.kt
@@ -3,24 +3,96 @@ package com.migc.fetchrewards
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.migc.fetchrewards.ui.theme.FetchRewardsTheme
+import com.migc.fetchrewards.ui.theme.Typography
+import com.migc.fetchrewards.ui.theme.backgroundColor
+import com.migc.fetchrewards.ui.theme.mainColor
 import org.koin.androidx.compose.koinViewModel
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val viewModel: MainViewModel = koinViewModel()
+            val items = viewModel.items.collectAsState()
+
             FetchRewardsTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    LazyColumn(modifier = Modifier
+                        .fillMaxSize()
+                        .background(backgroundColor)) {
+                        items.value.forEach { group ->
+                            stickyHeader {
+                                Surface(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    color = mainColor,
+                                    shadowElevation = 2.dp
+                                ) {
+                                    Row(
+                                        modifier = Modifier.fillMaxWidth(),
+                                        horizontalArrangement = Arrangement.Center
+                                    ) {
+                                        Text(
+                                            text = stringResource(id = R.string.list_id_text),
+                                            fontSize = Typography.titleLarge.fontSize
+                                        )
+                                        Spacer(modifier = Modifier.width(1.dp))
+                                        Text(
+                                            text = group.key.toString(),
+                                            fontSize = Typography.titleLarge.fontSize,
+                                            fontWeight = FontWeight.SemiBold
+                                        )
+                                    }
+                                }
 
+                            }
+                            items(group.value) { item ->
+                                Row(
+                                    horizontalArrangement = Arrangement.SpaceEvenly
+                                ) {
+                                    Text(
+                                        modifier = Modifier.weight(1f),
+                                        text = item.id.toString(),
+                                        textAlign = TextAlign.End
+                                    )
+                                    Spacer(modifier = Modifier.width(16.dp))
+                                    Text(
+                                        modifier = Modifier.weight(2f),
+                                        text = item.name.toString()
+                                    )
+                                }
+                            }
+                            item { 
+                                Spacer(modifier = Modifier.height(8.dp))
+                            }
+
+                        }
+                    }
                 }
             }
         }
--- app/src/main/java/com/migc/fetchrewards/MainViewModel.kt
+++ app/src/main/java/com/migc/fetchrewards/MainViewModel.kt
@@ -2,18 +2,25 @@ package com.migc.fetchrewards
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.migc.fetchrewards.data.ItemDto
 import com.migc.fetchrewards.domain.repository.Repository
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class MainViewModel(
     private val repository: Repository
 ) : ViewModel() {
 
+    private val _items: MutableStateFlow<Map<Int, List<ItemDto>>> = MutableStateFlow(emptyMap())
+    val items: StateFlow<Map<Int, List<ItemDto>>> = _items.asStateFlow()
+
     init {
         viewModelScope.launch(Dispatchers.IO) {
-            val items = repository.getItems()
-            items.forEach { println("${it.value}") }
+            _items.value = repository.getItems()
+            _items.value.forEach { println("${it.value}") }
         }
     }
 
